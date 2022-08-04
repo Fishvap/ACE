792ef502c2ca8dec374e00dabbc7e383c0433180
Fishvap <fishvap@vap.fish>
2022-08-04 04:18:51 +0100

    S&box content uploaded
    NO WAY.

---
+++ code/Game.cs
@@ -0,0 +1,41 @@
+﻿using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+//
+// You don't need to put things in a namespace, but it doesn't hurt.
+//
+namespace Sandbox;
+
+/// <summary>
+/// This is your game class. This is an entity that is created serverside when
+/// the game starts, and is replicated to the client. 
+/// 
+/// You can use this to create things like HUDs and declare which player class
+/// to use for spawned players.
+/// </summary>
+public partial class MyGame : Sandbox.Game
+{
+	// Variable definitions
+	var table_radius = 128;
+	var table_diameter = table_radius/2;
+
+	public MyGame()
+	{
+	}
+
+	/// <summary>
+	/// A client has joined the server. Make them a pawn to play with
+	/// </summary>
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+	}
+	public override void DoPlayerNoclip( Client player )
+	{
+		return;
+	}
+}
---
+++ code/Pawn.cs
@@ -0,0 +1,35 @@
+﻿using Sandbox;
+using System;
+using System.Linq;
+
+namespace Sandbox;
+
+// Variable definitions
+
+
+partial class Pawn : AnimatedEntity
+{
+	/// <summary>
+	/// Called when the entity is first created 
+	/// </summary>
+	public override void Spawn()
+	{
+		base.Spawn();
+	}
+
+	/// <summary>
+	/// Called every tick, clientside and serverside.
+	/// </summary>
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+	}
+
+	/// <summary>
+	/// Called every frame on the client
+	/// </summary>
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+	}
+}
